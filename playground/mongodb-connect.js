c99442570dd521cb0f35aad0cdc17448e5d6b3f0
Cuong Le <ltcuong2202@gmail.com>
2017-11-10 00:17:49 +0700

    add GET /todos:id

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -27,6 +27,6 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   //   }
   //   console.log(result.ops[0]._id.getTimestamp());
   // });
-  //
+
   db.close();
 });
--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const port = process.env.PORT || 3000;
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {ObjectId} = require('mongodb');
 
 var app = express();
 
@@ -28,6 +29,23 @@ app.get('/todos', (req,res) => {
   },(e) => {
     res.status(400).send(e);
   });
+});
+
+app.get('/todos/:id', (req,res) => {
+  var id = req.params.id;
+
+  if(!ObjectId.isValid(id)) {
+    return res.status(404).send(id);
+  }
+
+  Todo.findById(id).then((todo) => {
+    if(!todo) {
+      res.status(404).send();
+    }
+    res.send({todo});
+  }, (e) => {
+    res.status(400).send(e);
+});
 });
 
 app.listen(3000, () => {
